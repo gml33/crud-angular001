04b67d1f4a43b130363a3edeacd3ab2f249721f7
Marcelo Bruzzone <bruzzone1804@gmail.com>
2022-05-09 12:52:56 -0300

    preparando la primer api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonesService } from './servicios/pokemones.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rangular';
+
+  constructor(
+    private pokemonesService:PokemonesService
+  ){
+this.pokemonesService.getPokemones().subscribe((resp:any)=>{
+  this.pokemonesService = resp
+})
+  }
 }
